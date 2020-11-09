8490ddafacf52ac2a85d2d2c5e7db49c1e68356e
Wesley Silva <wesleysilva059@gmail.com>
2020-11-09 09:47:39 -0300

    First Commit

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //id ,
+            //customer_name 
+            //created_at
+            //CONSTRAINT orders_pkey PRIMARY KEY (id)
+        ];
+    }
+}
